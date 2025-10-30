95c84e9fd4c3b455656d9ce232362a77cb4d7f1d
DimpleKB <dimplekb23cs@rnsit.ac.in>
2025-10-30 22:11:10 +0530

    fix: render build command for frontend copy

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
